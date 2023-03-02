56a62ac4eea0be75330eefd3b741759f593d8102
macsteve4 <71797763+macsteve4@users.noreply.github.com>
2023-03-02 09:00:49 -0500

    Content addition for other "Check" exercises, established Swift views with built out buttons to prepare for Segue additions this week.

--- idm362-sam646/ViewController.swift
+++ idm362-sam646/ViewController.swift
@@ -9,6 +9,8 @@ import UIKit
 
 class ViewController: UIViewController {
     
+    
+    
     override var preferredStatusBarStyle: UIStatusBarStyle {
         .lightContent
     }
---
+++ idm362-sam646/ViewControllerCheck.swift
@@ -0,0 +1,25 @@
+//
+//  ViewControllerCheck.swift
+//  PostureCheck
+//
+//  Created by Steven M. on 3/1/23.
+//
+
+import UIKit
+
+class ViewControllerCheck: UIViewController {
+
+    @IBOutlet weak var timerCount: UILabel!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+    
+    @IBAction func timerButton(_ sender: Any) {
+        
+    }
+    
+}
