a7fcdaaf5eb7bfdbb557c3011190b604855f4d69
macsteve4 <71797763+macsteve4@users.noreply.github.com>
2023-03-25 04:31:10 -0400

    Interface Tweaks
    - margin edits
    - cymbal image edits

--- idm362-sam646/ViewControllerCheck.swift
+++ idm362-sam646/ViewControllerCheck.swift
@@ -10,46 +10,10 @@ import CoreData
 
 class ViewControllerCheck: UIViewController {
     
-    
-    var vCounter:Int = 0
-    
-    @IBAction func addstreakButton(_ sender: Any) {
-        vCounter += 1
-        /* // make this somehow passthrough
-        streaknumberLabel.text = String
-         */
-    }
-    
-    // Link Check core data to the checkobj
-    var checkObj: [NSManagedObject] = []
-    
     override func viewDidLoad() {
         super.viewDidLoad()
         
         // Do any additional setup after loading the view.
     }
     
-    
-    override func viewWillAppear(_ animated: Bool) {
-        super.viewWillAppear(animated)
-        
-        guard let appDelegate = UIApplication.shared.delegate as? AppDelegate else {
-            return
-        }
-        
-        let managedContent = appDelegate.persistentContainer.viewContext
-        
-        let fetchRequest = NSFetchRequest<NSManagedObject>(entityName: "Check")
-        
-        do {
-            checkObj = try managedContent.fetch(fetchRequest)
-        } catch let error as NSError {
-            print("Could not fetch. \(error.userInfo)")
-        }
-        
-    }
-    
-    
-    
 }
-
